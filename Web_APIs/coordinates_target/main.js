9b5f27c79f18d05effb8154169969b63da8687dd
Minho Kim <music4537@naver.com>
2021-06-02 12:58:24 +0900

    coordinates project

---
+++ Web_APIs/coordinates_target/main.js
@@ -0,0 +1,21 @@
+const vertical = document.querySelector('.vertical');
+const horizontal = document.querySelector('.horizontal');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
+
+document.addEventListener('mousemove', event => {
+    const x = event.clientX;
+    const y = event.clientY;
+    
+    tag.innerHTML = `
+        ${x}px , ${y}px
+    `;
+
+    vertical.style.left = `${x}px`;
+    horizontal.style.top = `${y}px`;
+    target.style.left = `${x}px`;
+    target.style.top = `${y}px`;
+    tag.style.left = `${x}px`;
+    tag.style.top = `${y}px`;
+})
+
