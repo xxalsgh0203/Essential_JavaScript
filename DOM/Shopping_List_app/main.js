32134f76b877cfe09285b9f3159ec84a6f8f590e
Minho Kim <music4537@naver.com>
2021-06-04 16:36:14 +0900

    DOM practice: Shopping list app

--- DOM/Shopping_List_app/main.js
+++ DOM/Shopping_List_app/main.js
@@ -7,11 +7,18 @@ const addBtn = document.querySelector('.footer_button');
 function onAdd(){
     // 1. 사용자가 입력한 텍스트를 받아옴
     const text = input.value;
+    if(text === ''){
+        input.focus();
+        return;
+    }
     // 2. 새로운 아이템을 만듬(텍스트 + 삭제버튼)
     const item = createItem(text);
     // 3. items 컨테이너에 새로 만든 아이템을 추가한다
     items.appendChild(item);
-    // 4. 인풋을 초기화 한다.
+
+    // 4. 새로 추가된 아이템으로 스크롤링
+    item.scrollIntoView({block: 'center'});
+    // 5. 인풋을 초기화 한다.
     input.value = '';
     input.focus();
 }
@@ -33,7 +40,7 @@ function createItem(text){
     deleteBtn.addEventListener('click', () => {items.removeChild(itemRow)});
     
     const itemDivider = document.createElement('div');
-    itemDivider.setAttribute('class', 'item_divider');
+    itemDivider.setAttribute('class', 'item_divder');
 
     item.appendChild(itemName);
     item.appendChild(deleteBtn);
@@ -45,6 +52,10 @@ function createItem(text){
 }
 
 addBtn.addEventListener('click', () => {
-    
     onAdd();
+});
+
+input.addEventListener('keypress', event => {
+    if(event.key === 'Enter')
+        onAdd();
 });
